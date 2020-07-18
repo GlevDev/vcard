860f74f043528fc169d077a7065d39b3acfba414
GlevDev <garth@glev.fr>
2020-07-18 22:56:26 +0200

    beginning form

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,20 @@
 <script>
   import Navbar from "./Sections/Navbar.svelte";
+
+  let disabled = true;
+
+  function keyPressed() {
+    disabled = false;
+  }
+
+  function resetClicked() {
+    disabled = true;
+  }
 </script>
 
-<Navbar title="vCard Generator"/>
+<Navbar title="vCard Generator" />
+
+<form>
+ <label>Name <input type="text" placeholder="Please enter your name" on:keydown={keyPressed}></label>
+ <button type="reset" {disabled} on:click={resetClicked}>Reset</button>
+</form>
